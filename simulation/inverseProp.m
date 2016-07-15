b1212df1a356e252761330a6473d85442bb02076
Jingzhao <zhangjingzhao@berkeley.edu>
2016-07-15 13:40:48 -0700

    Remove simulations

--- simulation/inverseProp.m
+++
@@ -1,9 +0,0 @@
-function [ phase ] = inverseProp( imageField, Nx, Ny, dz, lambda, ps )
-%INVERSEPROP Summary of this function goes here
-%   Detailed explanation goes here
-    regularizer = 1e-6;
-    Hstack = GenerateFresnelPropagationStack(Nx,Ny,dz, lambda, ps);
-    objectField = ifft2(ifftshift(imageField))./(Hstack + regularizer);
-    phase = angle(objectField);
-end
-
